c8f501a7560e31ff6b75234b07dd127663a18989
Chinthaka Manathunga <Chinthaka.Manathunga@lseg.com>
2024-02-29 01:45:32 +0530

    User, Roles and User Roles Changes
    Signed-off-by: Chinthaka Manathunga <Chinthaka.Manathunga@lseg.com>

--- src/main/java/lk/channelling/config/SecurityConfig.java
+++ src/main/java/lk/channelling/config/SecurityConfig.java
@@ -17,12 +17,57 @@ package lk.channelling.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().anyRequest();
+    public InMemoryUserDetailsManager userDetailService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder().encode("user1Pass"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("user2")
+                .password(passwordEncoder().encode("user2Pass"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("adminPass"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("/login", "/login/**", "/", "/index", "/home", "webjars/**", "/javascript/**", "*.css", "/assets/**", "/static/**")
+                                .permitAll().anyRequest().authenticated())
+                .formLogin(login -> login.loginPage("/login")
+                        .defaultSuccessUrl("/index", true)
+                        .failureUrl("/login?error=true"))
+                .logout(logout -> logout.logoutUrl("/logout")
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessUrl("/index").permitAll());
+
+        return http.build();
+
     }
 }
