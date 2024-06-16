dbbe3f041c718c8c361d1562f36a4bd81e594ef9
Chinthaka Manathunga <Chinthaka.Manathunga@lseg.com>
2024-06-16 12:38:21 +0530

    [CAA-12] Implement Doctor Speciality Rest APIs
    Signed-off-by: Chinthaka Manathunga <Chinthaka.Manathunga@lseg.com>

---
+++ src/main/java/lk/channelling/services/impl/DoctorSpecialityServiceImpl.java
@@ -0,0 +1,62 @@
+package lk.channelling.services.impl;
+
+import lk.channelling.entity.DoctorSpeciality;
+import lk.channelling.exception.RecordNotFoundException;
+import lk.channelling.repository.DoctorSpecialityRepository;
+import lk.channelling.services.DoctorSpecialityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class DoctorSpecialityServiceImpl implements DoctorSpecialityService {
+
+    private final DoctorSpecialityRepository doctorSpecialityRepository;
+
+    @Autowired
+    public DoctorSpecialityServiceImpl(DoctorSpecialityRepository doctorSpecialityRepository) {
+        this.doctorSpecialityRepository = doctorSpecialityRepository;
+    }
+
+    @Override
+    public List<DoctorSpeciality> findAll() {
+        return doctorSpecialityRepository.findAll();
+    }
+
+    @Override
+    public DoctorSpeciality findById(Long id) {
+        Optional<DoctorSpeciality> doctorSpeciality = doctorSpecialityRepository.findById(id);
+        if (doctorSpeciality.isEmpty()) {
+            throw new RecordNotFoundException("Doctor Speciality not found with id: " + id);
+        }
+        return doctorSpeciality.get();
+    }
+
+    @Override
+    public DoctorSpeciality save(DoctorSpeciality doctorSpeciality) {
+        return doctorSpecialityRepository.save(doctorSpeciality);
+    }
+
+    @Override
+    public void delete(Long id) {
+        doctorSpecialityRepository.deleteById(id);
+    }
+
+    @Override
+    public DoctorSpeciality update(Long id, DoctorSpeciality doctorSpeciality) {
+        Optional<DoctorSpeciality> existingDoctorSpeciality = doctorSpecialityRepository.findById(id);
+        if (existingDoctorSpeciality.isEmpty()) {
+            throw new RecordNotFoundException("Doctor Speciality not found with id: " + id);
+        }
+
+        DoctorSpeciality updatedDoctorSpeciality = existingDoctorSpeciality.get();
+        updatedDoctorSpeciality.setDoctorId(doctorSpeciality.getDoctorId());
+        updatedDoctorSpeciality.setSpecialityId(doctorSpeciality.getSpecialityId());
+
+        return doctorSpecialityRepository.save(updatedDoctorSpeciality);
+    }
+}
